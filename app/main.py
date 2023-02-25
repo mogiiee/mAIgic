199ffad3f61e5b06f76cf027380325fc6357ce02
mogiiee <76589641+mogiiee@users.noreply.github.com>
2023-02-25 22:53:20 +0530

    Update: cors functionality

--- app/main.py
+++ app/main.py
@@ -5,10 +5,20 @@ from pydantic import EmailStr
 from . import models, responses, ops, database
 from app.auth.jwt_bearer import JWTBearer
 from app.auth import jwt_handler
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
+origins = [""]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def greet():
